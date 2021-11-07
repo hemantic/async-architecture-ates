a69fb09d2b746031fd16855d016e17537d607f25
hemantic <artem.kiselev@gmail.com>
2021-11-07 16:23:41 +0300

    Добавлены заготовки для всех приложений на Django

---
+++ auth/src/app/models/user.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import UserManager as _UserManager
+from django.db import models
+
+
+class UserManager(_UserManager):
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, email, password, **extra_fields)
+
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    objects = UserManager()
+
+    REQUIRED_FIELDS = []
